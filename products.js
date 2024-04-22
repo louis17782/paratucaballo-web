c3b3c307fc159f2fe8d77f8b94615e572c817923
rpire <rubenpire7@gmail.com>
2024-04-21 21:12:35 -0400

    Crear productos.js
    Crear productos.js para almacenar la información de los productos ofrecidos por Para Tu Caballo.

---
+++ products.js
@@ -0,0 +1,9 @@
+const products = [
+  {
+    id: 1,
+    name: 'Desodorante 100ml',
+    desc: 'Este es un desodorante para caballos o gente muy hedionda.',
+    precioRef: 5000,
+    category: 'antibiotics',
+  },
+];
