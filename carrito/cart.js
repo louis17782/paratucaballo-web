5f50aa1755ee83be529d7535b4af258a7bce7778
rpire <rubenpire7@gmail.com>
2024-05-06 12:33:49 -0400

    Completar funciones de incrementar, disminuir y remover artículos del carrito

--- carrito/cart.js
+++ carrito/cart.js
@@ -1,30 +1,44 @@
-const cart = localStorage.getItem('carrito') ? JSON.parse(localStorage.getItem('carrito')) : null;
+// useCart es una función que busca al carrito en el almacén local, si no exite carrito en el almacén entonces devuelve un arreglo vacío.
+const useCart = () => localStorage.getItem('carrito') ? JSON.parse(localStorage.getItem('carrito')) : [];
 
+// Facil, esta función acepta el ID del item del que queremos disminuir la cantidad, busca al carrito con useCart y find para encontrar el item a modificar.
+// Una vez se verifica que la cantidad del item es mayor que 1 se modifica su cantidad, se actualiza el carrito y se refresca el DOM.
+const decreaseCartItemQuantity = (cartItemId) => {
+  const cart = useCart();
+
+  const cartItem = cart.find((cartItem) => cartItem.id === cartItemId);
+  if (cartItem.quantity > 1) cartItem.quantity -= 1;
+
+  localStorage.setItem('carrito', JSON.stringify(cart));
+  generateCart(true);
+};
+
+// Es lo mismo que el decrease pero se verifica que la cantidad del item sea menor a 100 y en vez de disminuir, aumenta la cantidad.
+const increaseCartItemQuantity = (cartItemId) => {
+  const cart = useCart();
+
+  const cartItem = cart.find((cartItem) => cartItem.id === cartItemId);
+  if (cartItem.quantity < 100) cartItem.quantity += 1;
+
+  localStorage.setItem('carrito', JSON.stringify(cart));
+  generateCart(true);
+};
+
+
+// Aquí hice algo bien chévere y es que filtro el carrito para remover únicamente el item cuyo ID es igual al del parámetro.
+// El elemento filtrado es removido del arreglo y luego es subido nuevamente al almacenamiento local.
+const removeCartItem = (cartItemId) => {
+  const cart = useCart();
+
+  const filteredCart = cart.filter((cartItem) => cartItem.id !== cartItemId);
+
+  localStorage.setItem('carrito', JSON.stringify(filteredCart));
+  generateCart(true);
+};
+
+// Esta función es medio tediosa, es generar un elemento en HTML para que muestre la información del artículo que se pasa en los parámetros.
+// Se hace verboso porque todo se genera utilizando JS, pero a la vez es muy práctico abstraer esta mecánica en una función para luego usarla.
 const generateCartItemHTML = (cartItem) => {
-  /*
-  <li class="cart-item">
-    <div class="cart-item-info">
-      <p class="product-title">Desodorante para caballos</p>
-      <p class="product-code">M0001</p>
-    </div>
-    <div class="cart-item-amounts">
-      <strong class="item-price">$500</strong>
-      <div class="amounts">
-        <button type="button" onclick="console.log('increase!')" class="cart-item-button">
-          <i class="fa-solid fa-plus"></i>
-        </button>
-        <span>5</span>
-        <button type="button" onclick="console.log('decrease!')" class="cart-item-button">
-          <i class="fa-solid fa-minus"></i>
-        </button>
-      </div>
-      <button type="button" onclick="console.log('quitar!')" class="remove-item">
-        QUITAR
-      </button>
-    </div>
-  </li>
-  */
-
   const listItem = document.createElement('li');
   listItem.classList.add('cart-item');
 
@@ -54,7 +68,7 @@ const generateCartItemHTML = (cartItem) => {
 
   const decreaseButton = document.createElement('button');
   decreaseButton.classList.add('cart-item-button');
-  decreaseButton.addEventListener('click', () => console.log('decrease!'))
+  decreaseButton.addEventListener('click', () => decreaseCartItemQuantity(cartItem.id));
 
   const minusSign = document.createElement('i');
   minusSign.classList.add('fa-solid', 'fa-minus');
@@ -65,14 +79,14 @@ const generateCartItemHTML = (cartItem) => {
 
   const increaseButton = document.createElement('button');
   increaseButton.classList.add('cart-item-button');
-  increaseButton.addEventListener('click', () => console.log('increase!'));
+  increaseButton.addEventListener('click', () => increaseCartItemQuantity(cartItem.id));
 
   const plusSign = document.createElement('i');
   plusSign.classList.add('fa-solid', 'fa-plus');
 
   const removeItemButton = document.createElement('button');
   removeItemButton.classList.add('remove-item');
-  removeItemButton.addEventListener('click', () => console.log('remove!'));
+  removeItemButton.addEventListener('click', () => removeCartItem(cartItem.id));
 
   const removeItemButtonText = document.createTextNode('QUITAR');
 
@@ -99,10 +113,16 @@ const generateCartItemHTML = (cartItem) => {
   return listItem;
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+// Esta función acepta un parámetro llamado `refresh` que por defecto es falso.
+// Si `refresh` es falso entonces simplemente toma la información del carrito y genera el HTML para cada artículo.
+// Si `refresh` es verdadero entonces primero borra todo el contenido del carrito en el DOM y vuelve a cargarlo con la info del carrito.
+const generateCart = (refresh = false) => {
   const container = document.getElementById('cart-container');
+  const cart = useCart();
 
-  if (cart) {
+  if (refresh) container.innerHTML = '';
+
+  if (cart.length > 0) {
     cart.forEach((cartItem) => container.appendChild(generateCartItemHTML(cartItem)));
   } else {
     const para = document.createElement('p');
@@ -112,4 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
     para.appendChild(message);
     container.appendChild(para);
   }
-});
+};
+
+// Este es un escuchador que ejecuta la función `generateCart` cuando el DOM ha terminado de cargar.
+document.addEventListener('DOMContentLoaded', generateCart);
